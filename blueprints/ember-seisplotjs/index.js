e22878874e083e079586d0f89f253c3f6b8bba97
Philip Crotwell <crotwell@seis.sc.edu>
2017-11-28 12:37:49 -0500

    add ember-moment

--- blueprints/ember-seisplotjs/index.js
+++ blueprints/ember-seisplotjs/index.js
@@ -20,7 +20,8 @@ module.exports = {
       // a packages array defines the addons to install
       packages: [
         // name is the addon name, and target (optional) is the version
-        {name: 'ember-browserify'}
+        {name: 'ember-browserify'},
+        {name: 'ember-moment'}
       ]
     })
     .then(() => {
