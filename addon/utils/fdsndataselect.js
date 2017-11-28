7ebebfdc371710d8e5df5925ff743a426f5bb0f6
Philip Crotwell <crotwell@seis.sc.edu>
2017-11-28 12:38:21 -0500

    use parenthesis!!!

--- addon/utils/fdsndataselect.js
+++ addon/utils/fdsndataselect.js
@@ -4,23 +4,19 @@ import seisplotjs from 'ember-seisplotjs';
 export default EmberObject.extend({
   init() {},
 
-
-  blaa() {
-    console.log("bla");
-  },
   load(channel, startTime, endTime) {
   console.log("in fdsndataselect util load: "+channel);
     if ( ! endTime) {
       endTime = new Date();
     }
     if (typeof startTime === 'number') {
-      if (!endTime instanceof Date) {
+      if ( ! (endTime instanceof Date)) {
         throw new Error("can't calculate times for s="+startTime+" e="+endTime);
       }
       startTime = new Date(endTime.getTime()-startTime*1000);
     }
     if (typeof endTime === 'number') {
-      if (!startTime instanceof Date) {
+      if ( ! (startTime instanceof Date)) {
         throw new Error("can't calculate times for s="+startTime+" e="+endTime);
       }
       endTime = new Date(startTime.getTime()+endTime*1000);
