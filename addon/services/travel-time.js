faf8122e700a3cdccba0d2300074d08522388e57
Philip Crotwell <crotwell@seis.sc.edu>
2018-10-18 09:11:23 -0400

    work on travetime

--- addon/services/travel-time.js
+++ addon/services/travel-time.js
@@ -1,7 +1,27 @@
 import Service from '@ember/service';
+import findProtocol from '../utils/find-protocol';
+import seisplotjs from 'ember-seisplotjs';
+const moment = seisplotjs.moment;
 
 export default Service.extend({
   init() {
     console.log("travel time service init");
+  },
+
+  load(quake, station, phaseList) {
+    if ( ! quake || ! station) {
+      throw new Error(`Both quake and station required q=${quake} s=${station}`);
+    }
+    if (! phaseList || phaseList.length === 0) {
+      phaseList = [ 'P', 'S'];
+    }
+    let distaz = seisplotjs.distaz(station.latitude, station.longitude,
+                                   quake.latitude, quake.longitude, quake.depth);
+    const query = new seisplotjs.traveltime.TraveltimeQuery();
+    query.protocol(findProtocol())
+      .evdepth(uake.depth)
+      .distdeg(distaz.delta)
+      .phases(phaseList.join(','));
+    return query.queryJson();
   }
 });
