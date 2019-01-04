cc4d0f2e0acd1763b42077fd0f006d4872dc1d04
Philip Crotwell <crotwell@seis.sc.edu>
2019-01-04 09:27:11 -0500

    work on add phases?

--- addon/services/travel-time.js
+++ addon/services/travel-time.js
@@ -13,15 +13,22 @@ export default Service.extend({
       throw new Error(`Both quake and station required q=${quake} s=${station}`);
     }
     if (! phaseList || phaseList.length === 0) {
-      phaseList = [ 'P', 'S'];
+      phaseList = [ 'p', 'P', 's', 'S', 'PKP', 'PKP', 'PKIKP', 'SKS'];
     }
     let distaz = seisplotjs.distaz(station.latitude, station.longitude,
                                    quake.latitude, quake.longitude, quake.depth);
     const query = new seisplotjs.traveltime.TraveltimeQuery();
     query.protocol(findProtocol())
-      .evdepth(uake.depth)
+      .evdepth(quake.depth)
       .distdeg(distaz.delta)
       .phases(phaseList.join(','));
-    return query.queryJson();
+    let promise = query.queryJson();
+    return RSVP.hash({
+      traveltime: promise,
+      distdeg: distaz.delta,
+      phaseList: phaseList,
+      quake: quake,
+      station: station
+    });
   }
 });
