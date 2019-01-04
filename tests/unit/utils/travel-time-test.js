8afbaaf4e03672e7a0c955fcf38ea0ec7b3f7738
Philip Crotwell <crotwell@seis.sc.edu>
2019-01-04 09:27:40 -0500

    traveltime as a util

---
+++ tests/unit/utils/travel-time-test.js
@@ -0,0 +1,11 @@
+import travelTime from 'dummy/utils/travel-time';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | travel-time', function(hooks) {
+
+  // Replace this with your real tests.
+  test('it works', function(assert) {
+    let result = travelTime();
+    assert.ok(result);
+  });
+});
