65c709f51ecb357fd2b641e6852da4b77e81aa98
Philip Crotwell <crotwell@seis.sc.edu>
2020-07-27 09:54:59 -0400

    indent as-json

--- addon/helpers/as-json.js
+++ addon/helpers/as-json.js
@@ -2,5 +2,5 @@ import { helper } from '@ember/component/helper';
 
 export default helper(function asJson(params/*, hash*/) {
   if (typeof params[0] === 'undefined') return "";
-  return JSON.stringify(params[0]);
+  return JSON.stringify(params[0], null, 2);
 });
