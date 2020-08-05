4e7af553dc9f2d2f7571789ef546b628b5462756
Philip Crotwell <crotwell@seis.sc.edu>
2020-08-05 09:59:14 -0400

    don't try to format non-numbers, just pass on

--- addon/helpers/format-number.js
+++ addon/helpers/format-number.js
@@ -4,5 +4,6 @@ export default helper(function formatNumber(params/*, hash*/) {
   if ( typeof params[0] === 'undefined') { return "";}
   let decimalDigits = params[1]===undefined ? 2 : params[1];
   if ( typeof params[0] === 'number') { return  params[0].toFixed(decimalDigits);}
-  throw new Error(`expected a number but received: ${typeof params[0]}`);
+  // not number, so just return unmodified
+  return params[0];
 });
